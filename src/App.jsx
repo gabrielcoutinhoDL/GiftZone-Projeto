da4f78c87d2aa395ec9fdba9ec47f1a39e5dc406
Ademir Arruda <ademirjose12340@gmail.com>
2025-11-22 03:21:07 -0300

    adição de cadastro e login (parte visual)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Anuncio from "./assets/Pages/anuncio/anuncio";
+import Login from "./assets/Pages/Login/Login.jsx";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Anuncio />} />
         <Route path="/anuncio" element={<Anuncio />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/assets/Pages/Login/Login.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react'
+import "./login.css";
+import {FaUser, FaEnvelope, FaLock} from "react-icons/fa";
+
+
+
+const Login = () => {
+
+    const [modo, setModo] = useState("login")
+
+
+  return (
+
+    <div className='login-container'>
+
+        <div className='card'>
+            
+            {/* adicionar logo depois */}
+            <div className='logo'>G</div>
+
+
+                <h1>Bem-vindo ao GiftZone</h1>
+                <p>Entre ou crie sua conta para continuar</p>
+        
+            <div className="tabs">
+
+                <button 
+                    className={modo === "login" ? "tab-active" : ""} 
+                    onClick={() => setModo("login")}
+                >
+                    Login
+                </button>
+
+                <button 
+                    className={modo === "cadastro" ? "tab-active" : ""} 
+                    onClick={() => setModo("cadastro")}
+                >
+                    Cadastro
+                </button>
+
+            </div>
+
+        </div>
+
+        {/* opção de login */}
+        {modo === "login" && (
+
+        <form className='form'>
+
+
+            <div>
+                <h2>Entrar na sua conta</h2>
+                <p>Digite seu e-mail e senha para acessar </p>
+            </div>
+
+
+            <div className="inputs">
+              <FaEnvelope className="icon" />
+              <input type="email" placeholder="Email" />
+            </div>
+
+
+            <div className="inputs">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Senha" />
+            </div>
+
+
+            <button className="btn-verde">Entrar</button>
+
+
+            <p className="termos">
+              Ao entrar, você concorda com nossos 
+              <span className='termos1'> Termos de Serviço e Política de Privacidade</span>
+            </p>
+
+        </form>
+
+        )}
+
+        {/*  opção de cadastro */}
+        {modo === "cadastro" && (
+
+        <form className='form'>
+
+            <div>
+                <h2>Criar nova conta</h2>
+                <p>Preencha os dados abaixo para se cadastrar</p>
+            </div>
+
+
+            <div className="inputs">
+              <FaUser className="icon" />
+              <input type="text" placeholder="Nome Completo" />
+            </div>
+
+
+            <div className="inputs">
+              <FaEnvelope className="icon" />
+              <input type="email" placeholder="Email" />
+            </div>
+
+
+
+
+            <div className="inputs">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Senha (mínimo 6 caracteres)" />
+            </div>
+
+
+
+
+            <div className="inputs">
+              <FaLock className="icon" />
+              <input type="password" placeholder="Confirmar Senha" />
+            </div>
+
+
+            <button className="btn-verde">Criar conta</button>
+
+
+            <p className="termos">
+              Ao criar uma conta, você concorda com nossos 
+              <span className='termos1'> Termos de Serviço e Política de Privacidade</span>
+            </p>
+
+
+        </form>
+
+        )}
+
+    </div>
+  )
+}
+
+export default Login;
