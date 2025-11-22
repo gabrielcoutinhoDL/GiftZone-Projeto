851c507b0a292c33120f8b4a1824fd0843ea514c
kaunferrer-web <kaunferrer@gmail.com>
2025-11-22 19:48:04 -0300

    pagina-principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Anuncio from "./assets/Pages/anuncio/anuncio";
 import Login from "./assets/Pages/Login/Login.jsx";
+import PaginaPrincipal from "./assets/Pages/Principal/PaginaPrincipal.jsx";
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/anuncio" element={<Anuncio />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<PaginaPrincipal/>}/>
       </Routes>
     </BrowserRouter>
   );
