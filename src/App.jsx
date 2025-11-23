8c598ceaa9cf2bc5fe6847af379949f8cb2ef13d
Gabriel Coutinho <165735404+gabrielcoutinhoDL@users.noreply.github.com>
2025-11-23 13:47:46 -0300

    Adição da page categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+//Componente pai
+
+//importa o componente filho
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Anuncio from "./assets/Pages/anuncio/anuncio";
 import Login from "./assets/Pages/Login/Login.jsx";
-
+import Categoria from "./assets/Pages/Categoria/categoria.jsx";
 
 export default function App() {
   return (
@@ -9,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/anuncio" element={<Anuncio />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/categoria" element={<Categoria />} />
       </Routes>
     </BrowserRouter>
   );
