a725528d37ad5b22f83bb9c1a2a3738499a34f76
Ademir Arruda <ademirjose12340@gmail.com>
2025-11-23 23:04:55 -0300

    Merge: Resolvendo conflito em Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Login from "./assets/Pages/Login/Login.jsx";
+import Login from "./assets/Pages/Login/login.jsx";
 import PaginaPrincipal from "./assets/Pages/Principal/PaginaPrincipal.jsx";
 import PageProdutos from "./assets/Pages/PageProdutos/PageProdutos.jsx";
 import Categoria from "./assets/Pages/Categoria/Categoria.jsx";
@@ -10,8 +10,8 @@ const App = () => {
   return (
     <div className="app-root">
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<PaginaPrincipal/>}/>
+        <Route path" element={<Login />} />
+        <Route path="/principal" element={<PaginaPrincipal/>}/>
         <Route path="/produto/:id" element={<PageProdutos/>} />
         <Route path="/categoria" element={<Categoria/>} />
       </Routes>
