9aa57c2138619e9e848ee822a6eeb0dcb2594a89
Gabriel Coutinho <165735404+gabrielcoutinhoDL@users.noreply.github.com>
2025-11-24 15:46:25 -0300

    adição pagamento no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./assets/Pages/Login/Login.jsx";
 import PaginaPrincipal from "./assets/Pages/Principal/PaginaPrincipal.jsx";
 import PageProdutos from "./assets/Pages/PageProdutos/PageProdutos.jsx";
 import Categoria from "./assets/Pages/Categoria/Categoria.jsx";
+import Pagamento from "./assets/Pages/pagamento/pagamento.jsx";
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<PaginaPrincipal/>}/>
         <Route path="/produto/:id" element={<PageProdutos/>} />
         <Route path="/categoria" element={<Categoria/>} />
+        <Route path="/pagamento" element={<Pagamento/>} />
       </Routes>
     </div>
   );
